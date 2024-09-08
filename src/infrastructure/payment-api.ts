416eb84ff1b05d5e6349fb81b7af4448b976fa1a
Martin Grandrath <martin.grandrath@codecentric.de>
2024-09-08 09:55:33 +0200

    add documentation and minor improvements to PaymentApi

--- src/infrastructure/payment-api.ts
+++ src/infrastructure/payment-api.ts
@@ -23,10 +23,24 @@ export type NullConfiguration = Record<CustomerId, Payment[]>;
 export class PaymentApiError extends Error {}
 
 export class PaymentApi implements IPaymentsApi {
+  // The `create` factory method creates an instance with the real side effect.
+  // In this case a real `HttpClient` instance.
   static create(configuration: PaymentApiConfiguration) {
     return new PaymentApi(configuration, HttpClient.create());
   }
 
+  // The `createNull` facory method creates an instance with a Null instance of
+  // the `HttpClient`. We also provide a default configuration for the
+  // `PaymentApi`.
+  //
+  // Notice that the Null configuration for the `PaymentApi` gets converted to
+  // the Null configuration for the `HttpClient` so that it behaves in way that
+  // in turn results in the desired behavior of the Null `PaymentApi` instance.
+  // This way tests that need a `PaymentApi` instance with a specific behavior
+  // don't have to know or care about the underlying implementation details. In
+  // principle this stack of higher level (app specific) classes using lower
+  // level (app independent) side effects can be arbitraryly deep. In practice
+  // there are usually no more than three layers.
   static createNull(nullConfiguration?: NullConfiguration) {
     return new PaymentApi(
       { baseUrl: new URL("https://example.com") },
@@ -44,17 +58,22 @@ export class PaymentApi implements IPaymentsApi {
     year: Year,
     month: Month
   ) {
+    // We use the injected `_httpClient` instance without knowing if it is the
+    // real one or the Nulled version. All the code in this method gets executed
+    // inside our tests.
     const response = await this._httpClient.sendRequest({
       method: "GET",
-      url: new URL(
-        `payments-by-month/${customerId}/${year}-${month
-          .toString()
-          .padStart(2, "0")}`,
-        this._configuration.baseUrl
-      ),
+      url: this.paymentsUrl(customerId, year, month),
     });
 
     try {
+      // We rigorously validate the data that we receive from the remote server
+      // at runtime. No amount of testing can prevent unexpected or erroneous
+      // behavior of a third party component outside our system. To account for
+      // this we never trust data that enters our system without validation. In
+      // the Nullables pattern this is known as Paranoic Telemetry[^1].
+      //
+      // [^1]: https://www.jamesshore.com/v2/projects/nullables/testing-without-mocks#paranoic-telemetry
       return ResponseSchema.parse(JSON.parse(response.body));
     } catch (error) {
       throw new PaymentApiError(
@@ -65,24 +84,26 @@ export class PaymentApi implements IPaymentsApi {
       );
     }
   }
+
+  private paymentsUrl(customerId: CustomerId, year: Year, month: Month) {
+    const paddedMonth = month.toString().padStart(2, "0");
+    return new URL(
+      `payments-by-month/${customerId}/${year}-${paddedMonth}`,
+      this._configuration.baseUrl
+    );
+  }
 }
 
+// The code for converting one Null configuration object into another is
+// arguably one of the uglier aspects of the Nullable pattern. We hide it down
+// here out of sight. ;-)
 const convertNullConfiguration = (
   nullConfiguration: NullConfiguration = {}
 ): HttpClientNullConfiguration => {
   const responses = Object.fromEntries(
     Object.entries(nullConfiguration).map(([idAndMonth, payments]) => {
-      const match = /^(.*)\/(\d{4})-(\d{1,2})$/.exec(idAndMonth);
-      if (!match) {
-        throw new Error(
-          `Invalid format. Expected "<customerId>/<year>-<month>" (ex. "customer-123/2020-05") but got "${idAndMonth}"`
-        );
-      }
-      const [_, customerId, year, month] = match;
       return [
-        `/payments-by-month/${customerId}/${year}-${month
-          ?.toString()
-          .padStart(2, "0")}`,
+        `/payments-by-month/${idAndMonth}`,
         {
           status: 200,
           body: JSON.stringify(payments),
