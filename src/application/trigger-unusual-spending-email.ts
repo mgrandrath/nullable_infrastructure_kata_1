97fbe38d4bb8e25279b4d21c5bf6c3a75b8b834b
Martin Grandrath <martin.grandrath@codecentric.de>
2024-10-10 09:36:12 +0200

    fix hardcoded test data

--- src/application/trigger-unusual-spending-email.ts
+++ src/application/trigger-unusual-spending-email.ts
@@ -6,16 +6,16 @@ import {
 import { ICalendar, IEmailService, IPaymentsApi } from "./interfaces";
 
 export const triggerUnusualSpendingEmail = async () => {
-  const currentMonth = { year: 2024, month: 11 };
   const previousMonth = { year: 2024, month: 10 };
+  const currentMonth = { year: 2024, month: 11 };
 
-  const currentPayments = [
+  const previousPayments = [
     { price: 99.99, category: "electronics", description: "irrelevant" },
     { price: 149.99, category: "electronics", description: "irrelevant" },
     { price: 29.99, category: "home", description: "irrelevant" },
     { price: 150.99, category: "clothing", description: "irrelevant" },
   ];
-  const previousPayments = [
+  const currentPayments = [
     { price: 70.89, category: "electronics", description: "irrelevant" },
     { price: 375.0, category: "electronics", description: "irrelevant" },
     { price: 39.99, category: "home", description: "irrelevant" },
